4f204fd0952929fb76142241e2de3d5f05144f53
soywiz <soywiz@gmail.com>
2018-02-15 16:24:06 +0100

    Workaround for Kotlin Native 0.6

--- klock/common/src/test/kotlin/com/klock/TimeDistanceTest.kt
+++ klock/common/src/test/kotlin/com/klock/TimeDistanceTest.kt
@@ -1,12 +1,18 @@
 package com.klock
 
-import com.soywiz.klock.DateTime
-import com.soywiz.klock.months
+import com.soywiz.klock.*
 import kotlin.test.*
 
 class TimeDistanceTest {
-	@Test
+	@kotlin.test.Test
 	fun testAddDistance() {
+		assertEquals(DateTime(2018, 12, 13), DateTime(2017, 12, 13) + 1.years)
 		assertEquals(DateTime(2018, 1, 13), DateTime(2017, 12, 13) + 1.months)
+		assertEquals(DateTime(2018, 1, 3), DateTime(2017, 12, 13) + 3.weeks)
+		assertEquals(DateTime(2018, 1, 2), DateTime(2017, 12, 13) + 20.days)
+		assertEquals(DateTime(2017, 12, 13, 3, 0, 0, 0), DateTime(2017, 12, 13) + 3.hours)
+		assertEquals(DateTime(2017, 12, 13, 0, 3, 0, 0), DateTime(2017, 12, 13) + 3.minutes)
+		assertEquals(DateTime(2017, 12, 13, 0, 0, 3, 0), DateTime(2017, 12, 13) + 3.seconds)
+		assertEquals(DateTime(2017, 12, 13, 0, 0, 0, 3), DateTime(2017, 12, 13) + 3.milliseconds)
 	}
 }
