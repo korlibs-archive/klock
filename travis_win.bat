da08b88216dcb2adc20099a427e6cbb1eaa0a6c5
soywiz <soywiz@gmail.com>
2018-10-14 05:17:33 +0200

    Try to manually remove unused stuff

--- travis_win.bat
+++ travis_win.bat
@@ -5,10 +5,14 @@ dir "c:\Program Files"
 dir "c:\Program Files (x86)"
 
 REM dir /s C:\ProgramData\chocolatey
-
 REM choco uninstall all
+REM choco uninstall -y -f cmake cmake.install DotNet4.5 DotNet4.6 windows-sdk-10.0 winscp winscp.install ruby microsoft-build-tools visualstudio2017-workload-netcorebuildtools visualstudio2017-workload-vctools visualstudio2017-workload-webbuildtools visualstudio2017buildtools
 
-choco uninstall -y -f cmake cmake.install DotNet4.5 DotNet4.6 windows-sdk-10.0 winscp winscp.install ruby microsoft-build-tools visualstudio2017-workload-netcorebuildtools visualstudio2017-workload-vctools visualstudio2017-workload-webbuildtools visualstudio2017buildtools
+RD /s /q "c:\Program Files\IIS"
+RD /s /q "c:\Program Files\Java"
+RD /s /q "c:\Program Files\Microsoft"
+RD /s /q "c:\Program Files\Microsoft Visual Studio"
+RD /s /q "c:\Program Files\Microsoft Visual Studio 14.0"
 
 dir
 
@@ -22,7 +26,7 @@ del /s C:\ProgramData\chocolatey\lib\ruby\
 dir c:\java8
 dir c:\java8\lib
 
-refreshenv
+CALL refreshenv
 
 SET JAVA_HOME=c:\java8
 CALL gradlew.bat -s -i mingwX64Test
