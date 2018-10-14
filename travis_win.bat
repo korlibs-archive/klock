047f90d226863a9b76c7fd0f72f315f159fc8309
soywiz <soywiz@gmail.com>
2018-10-14 04:34:46 +0200

    show more info

--- travis_win.bat
+++ travis_win.bat
@@ -3,5 +3,8 @@ dir
 choco list --local-only
 choco install jdk8 -y -params "installdir=c:\\java8"
 del c:\java8\lib\src.zip
+dir c:\java8
+dir c:\java8\lib
+
 SET JAVA_HOME=c:\java8
 CALL gradlew.bat -s -i mingwX64Test
