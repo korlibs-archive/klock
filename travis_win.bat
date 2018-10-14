df4fc80bbf1f5a577fdf880436aec05ac9d0f444
soywiz <soywiz@gmail.com>
2018-10-14 04:30:44 +0200

    try to figure out how to reduce used space

--- travis_win.bat
+++ travis_win.bat
@@ -1,6 +1,7 @@
 REM free some space
-choco uninstall jdk10 -y
-choco uninstall jdk11 -y
+dir
+choco list --local-only
 choco install jdk8 -y -params "installdir=c:\\java8"
+del c:\java8\lib\src.zip
 SET JAVA_HOME=c:\java8
 CALL gradlew.bat -s -i mingwX64Test
