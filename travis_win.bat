a90efcb61f3baa7f785a96bbc2eb75e9d1367fd8
soywiz <soywiz@gmail.com>
2018-10-14 04:42:58 +0200

    More guesses

--- travis_win.bat
+++ travis_win.bat
@@ -1,8 +1,16 @@
 REM free some space
 dir
+
+dir /s C:\ProgramData\chocolatey
+
+choco uninstall all
+
 choco list --local-only
 choco install jdk8 -y -params "installdir=c:\\java8"
-del c:\java8\lib\src.zip
+
+del c:\java8\src.zip
+del c:\java8\javafx-src.zip
+
 dir c:\java8
 dir c:\java8\lib
 
