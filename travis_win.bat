6f22ac2cf85f648cf631c86d1cf80b7e969c0828
soywiz <soywiz@gmail.com>
2018-10-14 04:15:45 +0200

    Try to fix params

--- travis_win.bat
+++ travis_win.bat
@@ -1,6 +1,6 @@
 REM free some space
 choco uninstall jdk10 -y
 choco uninstall jdk11 -y
-choco install jdk8 -y -params "source=false installdir=c:\\java8"
+choco install jdk8 -y -params "installdir=c:\\java8" -params "source=false"
 SET JAVA_HOME=c:\java8
 CALL gradlew.bat -s -i mingwX64Test
