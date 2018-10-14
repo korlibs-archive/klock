4aa41e3eb8d4305e78170b10b89e26e61a2a5c91
soywiz <soywiz@gmail.com>
2018-10-14 05:21:14 +0200

    Try to remove some more stuff

--- travis_win.bat
+++ travis_win.bat
@@ -13,6 +13,17 @@ RD /s /q "c:\Program Files\Java"
 RD /s /q "c:\Program Files\Microsoft"
 RD /s /q "c:\Program Files\Microsoft Visual Studio"
 RD /s /q "c:\Program Files\Microsoft Visual Studio 14.0"
+RD /s /q "c:\Program Files\cmake"
+RD /s /q "c:\Program Files\Microsoft SDKs"
+RD /s /q "c:\Program Files (x86)\IIS"
+RD /s /q "c:\Program Files (x86)\Java"
+RD /s /q "c:\Program Files (x86)\Microsoft"
+RD /s /q "c:\Program Files (x86)\Microsoft Visual Studio"
+RD /s /q "c:\Program Files (x86)\Microsoft Visual Studio 14.0"
+RD /s /q "c:\Program Files (x86)\cmake"
+RD /s /q "c:\Program Files (x86)\Microsoft SDKs"
+REM RD /s /q C:\ProgramData\chocolatey
+REM RD /s /q C:\ProgramData
 
 dir
 
@@ -21,7 +32,6 @@ choco install jdk8 -y -params "installdir=c:\\java8"
 
 del c:\java8\src.zip
 del c:\java8\javafx-src.zip
-del /s C:\ProgramData\chocolatey\lib\ruby\
 
 dir c:\java8
 dir c:\java8\lib
