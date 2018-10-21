25f1488f48f7372e7b1f885ccbb7e2e852fa5d2d
soywiz <soywiz@gmail.com>
2018-10-21 03:54:14 +0200

    Some measure adjustments and deprecations for migration

--- src/commonMain/kotlin/com/soywiz/klock/Measure.kt
+++ src/commonMain/kotlin/com/soywiz/klock/Measure.kt
@@ -1,18 +1,24 @@
 package com.soywiz.klock
 
-inline fun measureTimeMs(callback: () -> Unit): Int {
+inline fun measureTime(callback: () -> Unit): TimeSpan {
     val start = Klock.currentTimeMillisDouble()
     callback()
     val end = Klock.currentTimeMillisDouble()
-    return (end - start).toInt()
+    return (end - start).milliseconds
 }
 
 data class TimedResult<T>(val result: T, val time: TimeSpan)
 
-inline fun <T : Any> measureTime(callback: () -> T): TimedResult<T> {
+inline fun <T : Any> measureTimeWithResult(callback: () -> T): TimedResult<T> {
     lateinit var result: T
-    val ms = measureTimeMs {
+    val elapsed = measureTime {
         result = callback()
     }
-    return TimedResult(result, ms.milliseconds)
+    return TimedResult(result, elapsed)
 }
+
+@Deprecated("", ReplaceWith("measureTime(callback).millisecondsInt"))
+inline fun measureTimeMs(callback: () -> Unit): Int = measureTime(callback).millisecondsInt
+
+@Deprecated("", ReplaceWith("measureTimeWithResult(callback)"))
+inline fun <T : Any> measureTime(callback: () -> T): TimedResult<T> = measureTimeWithResult(callback)
