0038249e3b03eb8d39a2da0cf9df21c9f2bc7ed0
soywiz <soywiz@gmail.com>
2018-11-04 07:40:07 +0100

    Make TimeProvider interface + object

--- src/commonMain/kotlin/com/soywiz/klock/TimeProvider.kt
+++ src/commonMain/kotlin/com/soywiz/klock/TimeProvider.kt
@@ -1,13 +1,15 @@
 package com.soywiz.klock
 
 /** Class to provide time that can be overriden to mock or change its behaviour. */
-open class TimeProvider {
+interface TimeProvider {
     /** Returns a [DateTime] for this provider. */
-    open fun now(): DateTime = DateTime.now()
+    fun now(): DateTime
 
-    companion object {
+    companion object : TimeProvider {
+        override fun now(): DateTime = DateTime.now()
+
         /** Constructs a [TimeProvider] from a [callback] producing a [DateTime]. */
-        operator fun invoke(callback: () -> DateTime) = object : TimeProvider() {
+        operator fun invoke(callback: () -> DateTime) = object : TimeProvider {
             override fun now(): DateTime = callback()
         }
     }
