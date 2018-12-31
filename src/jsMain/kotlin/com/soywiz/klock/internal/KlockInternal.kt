49033949ff9cc670afe399c0d4c5034079fe6983
soywiz <soywiz@gmail.com>
2018-12-31 16:15:03 +0100

    Revert
    Signed-off-by: soywiz <soywiz@gmail.com>

--- src/jsMain/kotlin/com/soywiz/klock/internal/KlockInternal.kt
+++ src/jsMain/kotlin/com/soywiz/klock/internal/KlockInternal.kt
@@ -13,12 +13,12 @@ internal actual object KlockInternal {
     actual val currentTime: Double get() = (js("Date.now()").unsafeCast<Double>())
 
     actual val microClock: Double get() {
-        //return if (isNode) {
+        return if (isNode) {
             val result = process.hrtime(initialHrTime)
-            return (result[0] * 1_000_000 + floor((result[1] / 1_000) as Double)) as Double
-        //} else {
-        //    floor(window.performance.now() * 1000)
-        //}
+            (result[0] * 1_000_000 + floor((result[1] / 1_000) as Double)) as Double
+        } else {
+            floor(window.performance.now() * 1000)
+        }
     }
 
     actual fun localTimezoneOffsetMinutes(time: DateTime): TimeSpan {
