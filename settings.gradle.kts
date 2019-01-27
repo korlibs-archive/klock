3aa5fd471f3f94255ab313be6e989e91f3aef8f0
soywiz <soywiz@gmail.com>
2019-01-27 01:42:44 +0100

    Migrated gradle build to kotlin-dsl

---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+pluginManagement {
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "kotlin-multiplatform") {
+                useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:${requested.version}")
+            }
+        }
+    }
+
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        maven { url = uri("https://plugins.gradle.org/m2/") }
+        maven { url = uri("https://dl.bintray.com/kotlin/kotlin-eap") }
+    }
+}
+
+enableFeaturePreview("GRADLE_METADATA")
+
+rootProject.name = java.util.Properties().apply { load(File(rootProject.projectDir, "gradle.properties").readText().reader()) }.getProperty("project.name")
+
+include(":klock")
