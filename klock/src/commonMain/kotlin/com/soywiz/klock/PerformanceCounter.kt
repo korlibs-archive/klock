142c92fdaf24d856827cd1df8128449c9e5210d6
Carlos Ballesteros Velasco <soywiz@gmail.com>
2020-04-25 19:05:23 +0200

    Make PerformanceCounter an inline class that stores microseconds without decimal places, which should have better precision than TimeSpan

--- klock/src/commonMain/kotlin/com/soywiz/klock/PerformanceCounter.kt
+++ klock/src/commonMain/kotlin/com/soywiz/klock/PerformanceCounter.kt
@@ -5,24 +5,35 @@ import com.soywiz.klock.internal.*
 /**
  * Class for measuring relative times with as much precision as possible.
  */
-object PerformanceCounter {
-    /**
-     * Returns a performance counter measure in nanoseconds.
-     */
-    val nanoseconds: Double get() = KlockInternal.microClock * 1000.0
+inline class PerformanceCounter(val microseconds: Double) {
+    val nanoseconds: Double get() = microseconds * 1000.0
+    val milliseconds: Double get() = microseconds / 1000.0
+    val timespan: TimeSpan get() = this.microseconds.microseconds
 
-    /**
-     * Returns a performance counter measure in microseconds.
-     */
-    val microseconds: Double get() = KlockInternal.microClock
+    companion object {
+        /**
+         * Returns a performance counter measure in nanoseconds.
+         */
+        val nanoseconds: Double get() = KlockInternal.microClock * 1000.0
 
-    /**
-     * Returns a performance counter measure in milliseconds.
-     */
-    val milliseconds: Double get() = KlockInternal.microClock / 1000.0
+        /**
+         * Returns a performance counter measure in microseconds.
+         */
+        val microseconds: Double get() = KlockInternal.microClock
 
-    /**
-     * Returns a performance counter as a [TimeSpan].
-     */
-    val reference: TimeSpan get() = KlockInternal.microClock.microseconds
+        /**
+         * Returns a performance counter measure in milliseconds.
+         */
+        val milliseconds: Double get() = KlockInternal.microClock / 1000.0
+
+        /**
+         * Returns a performance counter as a [TimeSpan].
+         */
+        val reference: TimeSpan get() = KlockInternal.microClock.microseconds
+
+        /**
+         * Returns a performance counter as a [PerformanceCounter].
+         */
+        val counter: PerformanceCounter get() = PerformanceCounter(microseconds)
+    }
 }
