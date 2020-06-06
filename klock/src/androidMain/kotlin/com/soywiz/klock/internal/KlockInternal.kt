16c536af0a57e4934503f422c8ca0817f5702ae1
soywiz <soywiz@gmail.com>
2020-06-06 18:00:39 +0200

    Update android

--- klock/src/androidMain/kotlin/com/soywiz/klock/internal/KlockInternal.kt
+++ klock/src/androidMain/kotlin/com/soywiz/klock/internal/KlockInternal.kt
@@ -5,7 +5,7 @@ import java.util.*
 
 internal actual object KlockInternal {
     actual val currentTime: Double get() = (System.currentTimeMillis()).toDouble()
-    actual val microClock: Double get() = (System.nanoTime() / 1000L).toDouble()
+    actual val hrNow: HRTimeSpan get() = HRTimeSpan.fromNanoseconds(System.nanoTime().toDouble())
     actual fun localTimezoneOffsetMinutes(time: DateTime): TimeSpan = TimeZone.getDefault().getOffset(time.unixMillisLong).milliseconds
 }
 
