0367ab7985d56245f4508d3312d7a48464b74d4c
Carlos Ballesteros Velasco <soywiz@gmail.com>
2020-06-06 20:00:59 +0200

    TimesPerSecond / Frequency (#117)

---
+++ klock/src/commonMain/kotlin/com/soywiz/klock/Frequency.kt
@@ -0,0 +1,19 @@
+package com.soywiz.klock
+
+val TimeSpan.hz get() = timesPerSecond
+val Int.hz get() = timesPerSecond
+val Double.hz get() = timesPerSecond
+
+fun TimeSpan.toFrequency() = timesPerSecond
+
+val TimeSpan.timesPerSecond get() = Frequency(1.0 / this.seconds)
+val Int.timesPerSecond get() = Frequency(this.toDouble())
+val Double.timesPerSecond get() = Frequency(this)
+
+inline class Frequency(val hertz: Double) {
+    companion object {
+        fun from(timeSpan: TimeSpan) = timeSpan.toFrequency()
+    }
+
+    val timeSpan get() = (1.0 / this.hertz).seconds
+}
